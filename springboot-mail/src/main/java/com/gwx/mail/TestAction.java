6efd757b32ad0b7c02642caf4551dd96cc00af67
tom_glb <goodMorning_glb@atguigu.com>
2021-05-02 00:07:02 +0800

    init

---
+++ springboot-mail/src/main/java/com/gwx/mail/TestAction.java
@@ -0,0 +1,20 @@
+package com.gwx.mail;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: TestAction
+ * @Description:
+ * @Auther: Thomas
+ * @Version: 1.0
+ * @create 2021/5/1 22:34
+ */
+@RestController
+public class TestAction {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "This is a test";
+    }
+}
